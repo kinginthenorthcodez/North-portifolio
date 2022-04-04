15b4a95664123bf8d50bc4a41b6e39d1fb71f6c6
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-04-05 05:11:41 +0530

    Mobile Menu toggle
    Added menu-links to toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
 let mainMenu = document.querySelector("#main-menu");
 let btnMenu = document.querySelector("#btn-menu");
 let menuImg = document.querySelector("#menu-img");
+let menuItems = document.querySelectorAll('.menu-item a');
 
 btnMenu.addEventListener("click", function(e) {
+  swapIcon();
+  mainMenu.classList.toggle("mobile-hide")
+})
+
+for(let menuItem of menuItems){
+  menuItem.addEventListener('click', function(e){
+    mainMenu.classList.toggle('mobile-hide');
+    menuImg.src = "./assets/images/bars/IconMenu.png";
+  })
+}
+
+function swapIcon(){
   if (mainMenu.classList.contains("mobile-hide")) {
     menuImg.src = "./assets/images/bars/close.png";
   } else {
     menuImg.src = "./assets/images/bars/IconMenu.png";
   }
-  mainMenu.classList.toggle("mobile-hide")
-})
+}
