269973ae198a2b59ab86f48d6b2662995a326c79
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-04-05 05:53:08 +0530

    Fix linter errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,22 @@ const btnMenu = document.querySelector('#btn-menu');
 const menuImg = document.querySelector('#menu-img');
 const menuItems = document.querySelectorAll('.menu-item a');
 
-function swapIcon(){
+function swapIcon () {
   if (mainMenu.classList.contains('mobile-hide')) {
-    menuImg.src = "./assets/images/bars/close.png";
+    menuImg.src = './assets/images/bars/close.png';
   } else {
-    menuImg.src = "./assets/images/bars/IconMenu.png";
+    menuImg.src = './assets/images/bars/IconMenu.png';
   }
 }
 
-btnMenu.addEventListener('click',() => {
+btnMenu.addEventListener('click', () => {
   swapIcon();
-  mainMenu.classList.toggle('mobile-hide')
-})
+  mainMenu.classList.toggle('mobile-hide');
+});
 
-for (let menuItem of menuItems) {
-  menuItem.addEventListener('click',() => {
+for (const menuItem of menuItems) {
+  menuItem.addEventListener('click', () => {
     mainMenu.classList.toggle('mobile-hide');
-    menuImg.src = "./assets/images/bars/IconMenu.png";
-  })
+    menuImg.src = './assets/images/bars/IconMenu.png';
+  });
 }
-
-
