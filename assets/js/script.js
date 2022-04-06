fddffce3e2cb6e5adcbd75eade522d34310f81fb
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-04-07 04:05:55 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,8 +17,8 @@ const projects = [
     image:
       'https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=815&q=80',
     technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'My SQL'],
-    link: 'https://aimalamiri.github.io/Microverse-Portfolio/',
-    source: 'https://github.com/aimalamiri/Microverse-Portfolio',
+    link: 'https://kinginthenorthcodez.github.io/North-portifolio/',
+    source: 'https://github.com/kinginthenorthcodez/North-portifolio',
   },
   {
     id: 2,
@@ -28,8 +28,8 @@ const projects = [
     image:
       'https://images.unsplash.com/photo-1542744173-05336fcc7ad4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1102&q=80',
     technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby'],
-    link: 'https://aimalamiri.github.io/Microverse-Portfolio/',
-    source: 'https://github.com/aimalamiri/Microverse-Portfolio',
+    link: 'https://kinginthenorthcodez.github.io/North-portifolio/',
+    source: 'https://github.com/kinginthenorthcodez/North-portifolio',
   },
   {
     id: 3,
@@ -39,8 +39,8 @@ const projects = [
     image:
       'https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
     technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby'],
-    link: 'https://aimalamiri.github.io/Microverse-Portfolio/',
-    source: 'https://github.com/aimalamiri/Microverse-Portfolio',
+    link: 'https://kinginthenorthcodez.github.io/North-portifolio/',
+    source: 'https://github.com/kinginthenorthcodez/North-portifolio',
   },
   {
     id: 4,
@@ -49,8 +49,8 @@ const projects = [
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptatibus aliquid suscipit odit, eveniet nisi impedit nam placeat labore excepturi nulla facere ratione cupiditate culpa iure vel aliquam eum dolore.',
     image: 'https://i.pinimg.com/originals/6e/4b/2e/6e4b2e5e016ee15a405be4e09decfdc9.png',
     technologies: ['HTML', 'CSS', 'JavaScript'],
-    link: 'https://aimalamiri.github.io/Microverse-Portfolio/',
-    source: 'https://github.com/aimalamiri/Microverse-Portfolio',
+    link: 'https://kinginthenorthcodez.github.io/North-portifolio/',
+    source: 'https://github.com/kinginthenorthcodez/North-portifolio',
   },
   {
     id: 5,
@@ -60,8 +60,8 @@ const projects = [
     image:
       'https://mldpshakfq9r.i.optimole.com/Q925PfU-6c8Cn91e/w:1200/h:683/q:auto/https://www.core365.co.uk/wp-content/uploads/2021/07/JLN-Website-Screenshot.jpg',
     technologies: ['HTML', 'CSS', 'JavaScript'],
-    link: 'https://aimalamiri.github.io/Microverse-Portfolio/',
-    source: 'https://github.com/aimalamiri/Microverse-Portfolio',
+    link: 'https://kinginthenorthcodez.github.io/North-portifolio/',
+    source: 'https://github.com/kinginthenorthcodez/North-portifolio',
   },
   {
     id: 6,
@@ -70,8 +70,8 @@ const projects = [
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptatibus aliquid suscipit odit, eveniet nisi impedit nam placeat labore excepturi nulla facere ratione cupiditate culpa iure vel aliquam eum dolore.',
     image: 'https://i.pinimg.com/originals/ed/de/b9/eddeb936e3c74afa5b0182f847fb59fd.png',
     technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby'],
-    link: 'https://aimalamiri.github.io/Microverse-Portfolio/',
-    source: 'https://github.com/aimalamiri/Microverse-Portfolio',
+    link: 'https://kinginthenorthcodez.github.io/North-portifolio/',
+    source: 'https://github.com/kinginthenorthcodez/North-portifolio',
   },
   {
     id: 7,
@@ -81,8 +81,8 @@ const projects = [
     image:
       'https://img.webdesign-inspiration.com/v7/webdesign-inspiration.com/uploads/design/2019-05/v2-zopim-com-52981.png?width=488',
     technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby'],
-    link: 'https://aimalamiri.github.io/Microverse-Portfolio/',
-    source: 'https://github.com/aimalamiri/Microverse-Portfolio',
+    link: 'https://kinginthenorthcodez.github.io/North-portifolio/',
+    source: 'https://github.com/kinginthenorthcodez/North-portifolio',
   },
 ];
 
