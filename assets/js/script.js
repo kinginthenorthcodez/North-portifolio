5b4153fce128feaaac5e04b499028b3d5a49abd6
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-10-09 15:17:33 +0530

    Update linter files

--- assets/js/script.js
+++ assets/js/script.js
@@ -261,8 +261,7 @@ for (let i = 0; i < formInputs.length; i += 1) {
 
 const isRequired = (value) => value !== '';
 const isEmailValid = (email) => {
-  const reg =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return reg.test(email);
 };
 const msgError = document.querySelector('#msg-error');
